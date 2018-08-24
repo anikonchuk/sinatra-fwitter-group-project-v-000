0e2f7e826b4d1a897cc5687fb03a6b9d68454a7d
Andrew Nikonchuk <anikonch@gmail.com>
2018-08-24 21:00:15 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -37,6 +37,15 @@ class TweetsController < ApplicationController
     end
   end
 
+  patch '/tweets/:id' do
+    tweet = Tweet.find_by_id(params[:id])
+    tweet.content = params[:content]
+    if tweet.save
+      redirect "/tweets/#{tweet.id}"
+    else
+      redirect "/tweets/#{tweet.id}/edit"
+    end
+  end
 
   delete '/tweets/:id' do
     tweet = Tweet.find_by_id(params[:id])
